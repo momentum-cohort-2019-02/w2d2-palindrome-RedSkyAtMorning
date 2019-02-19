76bacfdbefef014cf4493ed39b8b034e29b2cb31
Jay Spain <meanderingjasmine@gmail.com>
2019-02-19 13:50:44 -0500

    file with useful name

---
+++ palindrome.py
@@ -0,0 +1,38 @@
+string = input("Give me a string: ")
+print("The string you gave is \"", string, "\"")
+length = len(string)
+print("the string is ",length, " characters long")
+
+
+
+def removespaces(stringToBeCleaned):
+    position = 0
+    clean = ""
+    while position < length:
+        cleanletter = stringToBeCleaned[position]
+        if cleanletter != " ":
+            clean = clean + cleanletter
+        position += 1
+    return clean
+
+cleanstring = removespaces(string)
+print("If you clean it, that would be \"",cleanstring,"\"")
+
+cleanlength = len(cleanstring)
+print("the cleaned string is ",cleanlength, "characters long")
+
+backwards = ""
+
+
+p = cleanlength - 1
+while p >= 0:
+    letter = cleanstring[p]
+    backwards = backwards + letter
+    p -= 1
+print("and if you make the cleanstring backwards you get \"",backwards,"\"")
+
+if backwards == cleanstring:
+    print ("You have a palindrome HURRAH")
+else: 
+    print("You do not have a palindrome. Try again.")
+
